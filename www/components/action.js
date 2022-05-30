d111cfec20876d27ff6ea0d1fa917f0c95fbdb6b
samarahelenabonifacio <samygrilin0721@gmail.com>
2022-05-30 23:03:04 +0900

    Project's Initial Commit

---
+++ www/components/action.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+
+    var url = "https://profrodolfo.com.br/projeto/";
+    
+    fetch(url)
+    .then(resposta => {
+        return resposta.json();
+    })
+    .then(function (json){
+        var d = document.querySelector('.dados');
+        var texto = '';
+        for(i = 0; i< json.length ; i++){
+            texto += '<div class="row">';
+            texto += '<div class="col-5">';
+            texto += '<img class="img-fluid" src="https://profrodolfo.com.br/projeto/'+json[i].foto+'">';
+            texto += '</div>';
+            texto += '<div class="col-7">';
+            texto += '<h1>'+json[i].nome+'</h1>';
+            texto += '<h3>'+json[i].valor+'</h3>';
+            texto += '</div>';
+            texto += '</div>';
+        }
+        d.innerHTML += texto;
+    })
+    .catch();
+}
